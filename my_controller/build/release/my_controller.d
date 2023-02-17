953b8fdc0a2a318a63e83473743155324778cc8d
ask-anuradha <ask.abhey@gmail.com>
2023-02-17 22:57:21 +0530

    sensors added

--- my_controller/build/release/my_controller.d
+++
@@ -1,5 +0,0 @@
-build/release/my_controller.o: my_controller.cpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Program\ Files/Webots/include/controller/c/webots/types.h
